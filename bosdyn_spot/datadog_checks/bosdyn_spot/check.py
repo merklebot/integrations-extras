de9d3bf49154ca3f8683845c424ba78305bc13a6
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-10-04 19:40:52 +0600

    Add battery charge percentage metric

--- bosdyn_spot/datadog_checks/bosdyn_spot/check.py
+++ bosdyn_spot/datadog_checks/bosdyn_spot/check.py
@@ -1,96 +1,32 @@
-
-from typing import Any
+import bosdyn.client
+from bosdyn.client.robot_state import RobotStateClient
 
 from datadog_checks.base import AgentCheck
 
-# from datadog_checks.base.utils.db import QueryManager
-# from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
-# from json import JSONDecodeError
-
 
 class BosdynSpotCheck(AgentCheck):
-
-    # This will be the prefix of every metric and service check the integration sends
     __NAMESPACE__ = 'bosdyn_spot'
 
     def __init__(self, name, init_config, instances):
         super(BosdynSpotCheck, self).__init__(name, init_config, instances)
 
-        # Use self.instance to read the check configuration
-        # self.url = self.instance.get("url")
+        self.spot_address = self.instance.get("spot_address")
+        self.spot_user = self.instance.get("spot_user")
+        self.spot_password = self.instance.get("spot_password")
 
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
+    def _ensure_spot_conn(self):
+        self.spot_sdk = bosdyn.client.create_standard_sdk("DatadogAgent")
+        self.spot = self.spot_sdk.create_robot(self.spot_address)
+        self.spot.authenticate(self.spot_user, self.spot_password)
+        self.spot_state_client = self.spot.ensure_client(RobotStateClient.default_service_name)
 
     def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
+        try:
+            self._ensure_spot_conn()
+        except Exception as e:
+            self.service_check("can_connect", AgentCheck.CRITICAL, message=str(e))
+            return
+        self.service_check("can_connect", AgentCheck.OK)
 
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(self.url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
-
-        # except Timeout as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request timeout: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except (HTTPError, InvalidURL, ConnectionError) as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except JSONDecodeError as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="JSON Parse failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except ValueError as e:
-        #     self.service_check(
-        #         "can_connect", AgentCheck.CRITICAL, message=str(e)
-        #     )
-        #     raise
-
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
-
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
-
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
-
-        # If your check ran successfully, you can send the status.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
-        # self.service_check("can_connect", AgentCheck.OK)
-
-        # If it didn't then it should send a critical service check
-        self.service_check("can_connect", AgentCheck.CRITICAL)
+        state = self.spot_state_client.get_robot_state()
+        self.gauge("battery_states.0.charge_percentage", state.battery_states[0].charge_percentage.value)
